2c6abf33b16c7377e66d7073d951283d130091ce
Eric Steedly <ericsteedly@gmail.com>
2026-04-20 09:55:31 -0500

    Add dio interceptors for better debug visibility

--- lib/services/dio_client.dart
+++ lib/services/dio_client.dart
@@ -1,8 +1,16 @@
 import 'package:dio/dio.dart';
 
-final dio = Dio(
-  BaseOptions(
-    baseUrl: 'https://panamapi.dev',
-    headers: {'Content-Type': 'application/json'},
-  ),
-);
+final dio =
+    Dio(
+        BaseOptions(
+          baseUrl: 'https://panamapi.dev',
+          headers: {'Content-Type': 'application/json'},
+        ),
+      )
+      ..interceptors.add(
+        LogInterceptor(
+          requestBody: true,
+          responseBody: true,
+          error: true,
+        ),
+      );
